83f248099d78e00c5d8f760f8bacdba22694d9a9
Lenin Lawrence <llawrence@lantronix.com>
2018-08-23 19:34:16 +0530

    swagger added

--- swagger.js
+++ swagger.js
@@ -1,25 +1,30 @@
 'use strict';
 
 const swaggerTools = require('swagger-tools');
-const Path = require('path');
 const swaggerJSDoc = require('swagger-jsdoc');
-const swaggerDefinition = {
-    info: {
-        title: 'API DOCUMENTATION',
-        description: 'Whole Description Here',
-        version: 'Version 1'
-    },
-    basePath: '/api/v1',
-    schemes: ['http']
-};
+const fs = require('fs');
+const jsyaml = require('js-yaml');
+
+// const swaggerDefinition = {
+//     info: {
+//         // API informations (required)
+//         title: 'Hello World', // Title (required)
+//         version: '1.0.0', // Version (required)
+//         description: 'A sample API', // Description (optional)
+//       },
+//       host: 'localhost:3000', // Host (optional)
+//       basePath: '/', // Base path (optional)
+// };
+const spec = fs.readFileSync('./swagger.yaml', 'utf8');
+const swaggerDoc = jsyaml.safeLoad(spec);
 
 module.exports = app => {
-    const swaggerUntrustedRoutesSpecJSON = swaggerJSDoc({
-        swaggerDefinition,
-        apis: [Path.resolve(__dirname, '/routes/**/*_routes.js')]
-    });
-
-    swaggerTools.initializeMiddleware(swaggerUntrustedRoutesSpecJSON, (m) => {
+    // const swaggerUntrustedRoutesSpecJSON = swaggerJSDoc({
+    //     swaggerDefinition,
+    //     apis: ['./routes/auth_route.js']
+    // });
+    // console.log(swaggerUntrustedRoutesSpecJSON);
+    swaggerTools.initializeMiddleware(swaggerDoc, (m) => {
         app.use(m.swaggerUi());
     });
 };
